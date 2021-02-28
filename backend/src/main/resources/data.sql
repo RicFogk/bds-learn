a4a3e93a11bb02f291dc65dc71951b6c18099b4d
Ricardo Fogaca <fogaca135@gmail.com>
2021-02-28 16:32:08 -0300

    offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('BootCamp HTML', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ftraining-course-training-3250007%2F&psig=AOvVaw27sXiKzhMe0DRDwaIN3fF4&ust=1614608545114000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDBt9vjjO8CFQAAAAAdAAAAABAD', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ASwitch-course-book-grey.svg&psig=AOvVaw0Xj1LWQXOMOmJKD4u0yVjQ&ust=1614608585508000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiG3e7jjO8CFQAAAAAdAAAAABAQ');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
