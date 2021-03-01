d83eff7219b9ec9924d423dc22dcbea6948981b3
Ricardo Fogaca <fogaca135@gmail.com>
2021-03-01 09:20:18 -0300

    Enrollment, EnrollmentPK

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -0,0 +1,93 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+
+@Entity
+@Table(name = "tb_enrollment" ) 
+public class Enrollment {
+	
+	//quando é uma chave composta nao coloca como estavamos colocando em outras classes
+	
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK(); // vindo da tabela de asssociacoes // chave composto // **** NAO PODE ESQUECER DE INSTANCIAR
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	//Nao é colocado com Wrapper para garantir = tipo basico para garantir verdadeiro ou falso // Agora se tiver a possibilidade de ter a terceira opcao: null ai colocar o Boolean(Wrapper)
+	private boolean available;
+	private boolean onlyUpdate;
+	
+	public Enrollment() {
+	}
+
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+			boolean onlyUpdate) {
+		super();
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.available = available;
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	//criando na mao ao inves do GETTER/SETTERS do EnrollmentPK
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudent(User user) {
+		id.setUser(user);
+	}
+	
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+	
+	
+}
